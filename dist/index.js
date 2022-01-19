371a4a6c84eae9874af91df7ff0071ccca8e73e5
Michal Zagrodzki <michalzagrodzki@gmail.com>
2022-01-19 20:44:56 +0100

    feat: update dist

--- dist/index.js
+++ dist/index.js
@@ -1,1 +1,15 @@
 "use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+Object.defineProperty(exports, "Panel", {
+  enumerable: true,
+  get: function get() {
+    return _panel.default;
+  }
+});
+
+var _panel = _interopRequireDefault(require("./components/panel"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
